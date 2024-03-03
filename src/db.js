754bf2d2e29b6ccd4de38a1e435d5618027aaa1e
cor22982 <cor22982@uvg.edu.gt>
2024-03-02 19:28:51 -0600

    feat:Put get post id

--- src/db.js
+++ src/db.js
@@ -11,3 +11,8 @@ export async function createPost(title, content) {
   const [result] = await db.query('INSERT INTO blog_posts (title, content) VALUES (?, ?)', [title, content])
   return result
 }
+
+export async function getOnePost(postId) {
+  const [result] = await conn.query('SELECT * FROM blog_posts WHERE id = ?', [postId])
+  return result
+}
--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
 import express from 'express'
 // eslint-disable-next-line import/extensions
-import { getAllPosts } from './db.js'
+import { getAllPosts, getOnePost } from './db.js'
+// eslint-disable-next-line import/no-extraneous-dependencies
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.get('/posts', async (req, res) => {
   const posts = await getAllPosts()
-  res.json(posts)
+  res.status(200).json(posts)
+})
+app.get('/posts/:postId', async (req, res) => {
+  const { postId } = req.params
+  // eslint-disable-next-line no-undef
+  const post = await getOnePost(postId)
+  res.status(200).json(post)
 })
 
 const PORT = process.env.PORT || 3000
